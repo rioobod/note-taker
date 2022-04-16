a13cd1934d31585bf7b1070e38a8f03b7e435190
rioobod <akanbilola@gmail.com>
2022-04-16 12:13:59 -0500

    created handle request for notes

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const { notes } = require("./db/db.json");
+
+app.get("/api/notes", (req, res) => {
+  //  res.send('hello');
+     let results = notes;
+     console.log(req.query);
+
+  res.json(results);
+//   res.json(notes);
+});
+
+app.listen(3001, () => {
+  console.log(`API server now on port 3001!`);
+});
